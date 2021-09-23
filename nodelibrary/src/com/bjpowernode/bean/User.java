85978637df75aa0d7158861c568b33d88c2486de
njuptzx <58161418+njuptzx@users.noreply.github.com>
2021-09-23 16:51:01 +0800

    Add files via upload

---
+++ nodelibrary/src/com/bjpowernode/bean/User.java
@@ -0,0 +1,100 @@
+package com.bjpowernode.bean;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+
+    private String name;
+
+    //状态
+    private String status;
+
+    //余额
+    private BigDecimal money;
+
+    //表示当前用户是否借书
+    private boolean islend;
+
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && islend == user.islend && Objects.equals(name, user.name) && Objects.equals(status, user.status) && Objects.equals(money, user.money);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, status, money, islend);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", money=" + money +
+                ", islend=" + islend +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public boolean isIslend() {
+        return islend;
+    }
+
+    public void setIslend(boolean islend) {
+        this.islend = islend;
+    }
+
+    public User(int id, String name, String status, BigDecimal money, boolean islend) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.money = money;
+        this.islend = islend;
+    }
+
+}
